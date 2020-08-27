f22b09d4d34ee5fb8b0a726d929cb5666d22d69f
sagalpreet <sagalpreetsingh60@gmail.com>
2020-08-27 15:11:13 +0530

    Python script to obtain 2-d grayscale images composed of ascii characters

---
+++ 2d_Image.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import numpy as np
+
+image = Image.open('d:/z_temp/Annotation 2020-07-07 220912.png')
+imageArray = np.asarray(image)
+
+imageArray.shape
+
+image = image.resize((1000, int(1000 * int(imageArray.shape[0])/int(imageArray.shape[1]) ) ) )
+image.size
+imageArray = np.asarray(image)
+
+grayImageArray = np.array([[0 for i in range(imageArray.shape[1])] for j in range(imageArray.shape[0])])
+for i in range(imageArray.shape[0]):
+     for j in range(imageArray.shape[1]):
+        x = ( int(imageArray[i][j][0]) + int(imageArray[i][j][1]) + int(imageArray[i][j][2]) )
+        grayImageArray[i][j] = np.uint8(x // 3)
+
+grayLevels = '.:-=+*#%@'
+
+for i in range(imageArray.shape[0]):
+    for j in range(imageArray.shape[1]):
+        densityLevel = ( 9 * int(grayImageArray[i][j]) ) // 255
+        print(grayLevels[densityLevel], end='')
+    print()
